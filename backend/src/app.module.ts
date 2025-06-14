4f922ac097870352ae976f104b7879a43c151e77
Henrik VT <commits@henrikvt.com>
2025-06-13 20:12:33 -0400

    add new modules to app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { Drizzle } from './database/drizzle.provider';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Drizzle],
 })
 export class AppModule {}
