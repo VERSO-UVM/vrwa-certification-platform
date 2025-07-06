5b820ca5f505748f38c827b0eb86767a4b0b02db
Henrik VT <commits@henrikvt.com>
2025-07-06 00:32:09 -0400

    start implementing some AuthService stuff

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DrizzleModule } from 'src/database/drizzle.module';
 
 @Module({
+  imports: [DrizzleModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import {
+  DrizzleAsyncProvider,
+  type DrizzleProviderReturn,
+} from 'src/database/drizzle.provider';
+import { hash, verify } from '@node-rs/argon2';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(DrizzleAsyncProvider)
+    private db: DrizzleProviderReturn,
+  ) {}
+
+  async validateLoginRequest(email: string, password: string) {
+    const user = await this.db.client.query.user.findFirst({
+      where: (users, { eq, and }) =>
+        and(eq(users.email, email), eq(users.hasRegistered, true)),
+    });
+
+    if (!user || !user.passwordHash) {
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+
+    return AuthService.comparePasswordHashes(user.passwordHash, password);
+  }
+
+  private static async comparePasswordHashes(hash: string, password: string) {
+    return await verify(hash, password);
+  }
+
+  private static async hashPassword(password: string) {
+    return await hash(password);
+  }
+}
