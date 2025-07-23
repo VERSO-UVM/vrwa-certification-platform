feeab568f009c86238e23f9b164abe603f6cb09d
Henrik VT <commits@henrikvt.com>
2025-07-23 17:29:24 -0400

    move conditional session cookie declaration

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,1 +1,6 @@
-export const SESSION_COOKIE_NAME = 'vrwa-session';
+export const SESSION_COOKIE_BASENAME = 'vrwa-session';
+
+export const SESSION_COOKIE_NAME =
+  process.env.NODE_ENV?.toLocaleLowerCase() === 'production'
+    ? `__Secure-${SESSION_COOKIE_BASENAME}`
+    : SESSION_COOKIE_BASENAME;
