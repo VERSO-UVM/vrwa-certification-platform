4d86726cbe8786a7000e2dc752789500999a3f9f
Henrik VT <commits@henrikvt.com>
2025-07-23 17:27:58 -0400

    db setup

---
+++ backend/src/database/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './schema';
+import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const client = drizzle(pool, { schema }) as NodePgDatabase<typeof schema>;
+
+export { client, schema };
