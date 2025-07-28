5f4207ec0f7ed98d6f1832177cd93854858c91cb
Henrik VT <commits@henrikvt.com>
2025-07-28 11:44:14 -0400

    port all existing auth logic to tRPC

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,9 @@
+import { authRouter } from './auth/router';
 import { router } from './utils/trpc';
-export const appRouter = router({});
+export const appRouter = router({
+  authRouter,
+});
+
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
