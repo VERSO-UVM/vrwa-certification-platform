095db71357cfd7e7d3c27f1be8696d94dfccf744
Henrik VT <commits@henrikvt.com>
2025-07-28 11:43:49 -0400

    context and auth logic

--- backend/src/utils/trpc/index.ts
+++ backend/src/utils/trpc/index.ts
@@ -1,13 +1,39 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
+import type { Context } from './ctx';
+import { ZodError } from 'zod';
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.create();
+const t = initTRPC.context<Context>().create({
+  // transformer: superjson,
 
-/**
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
 export const router = t.router;
 export const publicProcedure = t.procedure;
+
+const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
+  if (!ctx.user || !ctx.session) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+  return next({
+    ctx: {
+      // infers the `user` and `session` as non-nullable
+      user: ctx.user,
+      session: ctx.session,
+    },
+  });
+});
+
+export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
