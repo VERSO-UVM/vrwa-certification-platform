1ce06c275297af0a15036bef832f20cf9d3bf8ed
sadieellwood <sellwood@uvm.edu>
2026-02-05 15:19:31 +0000

    new migrations for schema changes

---
+++ backend/drizzle/0003_left_zaladane.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "profile" ADD COLUMN "address" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "profile" ADD COLUMN "city" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "profile" ADD COLUMN "state" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "profile" ADD COLUMN "postalCode" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "profile" ADD COLUMN "phoneNumber" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "profile" ADD COLUMN "isMember" boolean NOT NULL;
