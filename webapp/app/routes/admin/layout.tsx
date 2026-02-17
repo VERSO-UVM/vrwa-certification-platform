e9b3320eee77228e21eb8535c8105bb9a6723427
Rustum Zia <ziarustum@gmail.com>
2026-02-17 16:54:28 +0100

    [admin] add sidebar component

--- webapp/app/routes/admin/layout.tsx
+++ webapp/app/routes/admin/layout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router";
+import { AdminSidebar } from "~/admin/navigation";
+import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 
 export default function Admin() {
   return (
     <div>
-      <Outlet />
+      <SidebarProvider>
+        <AdminSidebar />
+        <main>
+          <SidebarTrigger />
+          <Outlet />
+        </main>
+      </SidebarProvider>
     </div>
   );
 }
