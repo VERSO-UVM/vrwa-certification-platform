c145d7fb5390bf1f3e4fcf24e96ce5bb376ace3b
Rustum Zia <ziarustum@gmail.com>
2026-02-24 17:05:32 +0100

    Add untracked dtos.ts file

---
+++ backend/src/database/dtos.ts
@@ -0,0 +1,15 @@
+// This file defines the structures used in API responses. So this file is meant
+// to be imported (`import type` only) in both the server and the client. (These are
+// called DTOs (Data Transfer Objects))
+
+import type { Course, CourseEvent, Profile, Reservation } from './schema';
+
+export type ReservationDto =
+  | Reservation
+  | Pick<Profile, 'firstName' | 'lastName' | 'isMember'>
+  | Pick<Course, "courseName" | "creditHours">;
+
+
+export type CourseEventDto = 
+  | CourseEvent
+  | Pick<Course, "courseName" | "description" | "creditHours" | "priceCents">
