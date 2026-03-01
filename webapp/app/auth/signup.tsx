65acf69d0a974468f1a0244f5347f0dcbc0f864c
Rustum Zia <ziarustum@gmail.com>
2026-03-01 20:13:05 +0100

    (login and signup)

--- webapp/app/auth/signup.tsx
+++ webapp/app/auth/signup.tsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router";
-import { Card, CardTitle, CardContent } from "~/components/ui/card";
+import { Card, CardTitle, CardContent, CardFooter } from "~/components/ui/card";
 
 export default function SignupPage() {
   return (
     <Card className="mt-10 w-md self-center">
       <CardTitle className="text-center">Sign-up</CardTitle>
-      <CardContent>
-        Or, <Link to="/login" className="text-blue-700 underline">log in</Link>.
-      </CardContent>
+      <CardFooter className="flex items-start flex-col gap-4">
+        <div>
+          Or,&nbsp;
+          <Link to="/login" className="text-blue-700 underline">
+            log in
+          </Link>
+          .
+        </div>
+        <div>
+          <Link to="/" className="underline">
+            Exit
+          </Link>
+        </div>
+      </CardFooter>
     </Card>
   )
 }
