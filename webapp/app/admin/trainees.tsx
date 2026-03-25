b9f2193c461f18c6b0cda30bbbddd2f56f03b2fa
Rustum Zia <ziarustum@gmail.com>
2026-03-25 23:54:30 +0100

    feat(admin/trainees):

--- webapp/app/admin/trainees.tsx
+++ webapp/app/admin/trainees.tsx
@@ -1,15 +1,9 @@
 import type { Profile } from "@backend/database/schema";
 import { useQuery } from "@tanstack/react-query";
-import type {
-  ColumnDef,
-  RowSelectionState,
-  Updater,
-} from "@tanstack/react-table";
 import { Button } from "~/components/ui/button";
 import { DataTable } from "~/components/ui/data-table";
 import { PageHeader } from "~/components/page-header";
 import { TraineeReservations } from "./trainee-reservations";
-import { useState } from "react";
 import { useTRPC } from "~/utils/trpc";
 import { useReactTableRowSelect } from "~/hooks/use-row-select";
 import { profileColumnSets } from "~/utils/column-defs/profile";
@@ -34,7 +28,7 @@ export function TraineeManager() {
       <div className="flex flex-col space-y-1 max-w-5xl ">
         <div className="flex-1 border rounded p-3">
           <DataTable
-            columns={profileTableDef}
+            columns={profileColumnSets.default}
             data={trainees}
             table={{
               onRowSelectionChange: reactTableSelectionChange,
@@ -49,30 +43,7 @@ export function TraineeManager() {
             <h2 className="text-xl font-medium pb-4">
               {selectedTrainee.firstName} {selectedTrainee.lastName}
             </h2>
-            <ul className="flex flex-col space-y-2 pb-2">
-              <li>
-                <b>First Name(s):</b> {selectedTrainee.firstName}
-              </li>
-              <li>
-                <b>Last Name(s):</b> {selectedTrainee.lastName}
-              </li>
-              <li>
-                <b>Address:</b> {selectedTrainee.address}
-              </li>
-              <li>
-                <b>City:</b> {selectedTrainee.city}
-              </li>
-              <li>
-                <b>Postal Code:</b> {selectedTrainee.postalCode}
-              </li>
-              <li>
-                <b>Phone Number:</b> {selectedTrainee.phoneNumber}
-              </li>
-              <li>
-                <b>Is Member:</b> {selectedTrainee.isMember ? "Yes" : "No"}
-              </li>
-            </ul>
-
+            <DetailsDisplay item={selectedTrainee} columns={profileColumnSets.complete} />
             <Button>Edit</Button>
 
             <h2 className="text-lg font-medium py-4">Classes</h2>
