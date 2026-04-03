1e72e1cf3aebbcb82fb8ab5bf6226cebd561cc24
Rustum Zia <ziarustum@gmail.com>
2026-04-03 16:48:32 +0200

    trainee-manager

--- webapp/app/admin/trainee-manager/reservations.tsx
+++ webapp/app/admin/trainee-manager/reservations.tsx
@@ -1,20 +1,36 @@
 import type { ReservationDto } from "@backend/database/dtos";
 import { useQuery } from "@tanstack/react-query";
 import { DataTable } from "~/components/ui/data-table";
-import { useReactTableRowSelect } from "~/hooks/use-row-select";
 import { useTRPC } from "~/utils/trpc";
 import { EditTraineeReservation } from "./edit-reservation";
 import {
-  reservationColumnDefLists,
   reservationColumnHelper,
+  reservationColumnDefs,
 } from "~/utils/column-defs/reservation";
+import type { ColumnDef } from "@tanstack/react-table";
+import type { PaymentStatus } from "@backend/database/schema";
 
 const columnDefs = [
-  ...reservationColumnDefLists.basic,
+  reservationColumnDefs.firstName,
+  reservationColumnDefs.lastName,
+  reservationColumnDefs.courseName,
+  {
+    ...reservationColumnDefs.creditHours,
+    cell: ({ renderValue }) => (
+      <span className="font-bold">{renderValue()}</span>
+    ),
+  } satisfies ColumnDef<ReservationDto, string>,
+  {
+    ...reservationColumnDefs.paymentStatus,
+    cell: ({ renderValue }) => (
+      <span className="font-bold">{renderValue()}</span>
+    ),
+  } satisfies ColumnDef<ReservationDto, PaymentStatus>,
   reservationColumnHelper.display({
     header: "Actions",
     cell: ({ row }) => <EditTraineeReservation reservation={row.original} />,
   }),
+  reservationColumnDefs.classStartDateTime,
 ];
 
 export function TraineeReservations({ profileId }: { profileId: string }) {
@@ -26,19 +42,12 @@ export function TraineeReservations({ profileId }: { profileId: string }) {
   );
   const reservations = (query.data ?? []) as ReservationDto[];
 
-  const [[index], [rowSelection, onRowSelectionChange]] =
-    useReactTableRowSelect();
-  const selectedReservation = reservations[index] ?? null;
-
   return (
     <DataTable
       columns={columnDefs}
       data={reservations}
       table={{
-        onRowSelectionChange,
-        state: {
-          rowSelection,
-        },
+        enableRowSelection: false,
       }}
     />
   );
