b779ddeb8db405ac0d1b924938b946ec9eae27d8
Rustum Zia <ziarustum@gmail.com>
2026-06-23 11:20:58 -0400

    seasoning

--- webapp/app/utils/auth.ts
+++ webapp/app/utils/auth.ts
@@ -1,8 +1,12 @@
+/// Client-side part of better-auth.
+
 import { createAuthClient } from "better-auth/react";
 import type { auth } from "@backend/auth/server";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import { getBackendUrl } from "./env";
 
+// We need to be careful how we define this to not break
+// typescript inference
 export const authClient = createAuthClient({
   baseURL: getBackendUrl(),
   plugins: [inferAdditionalFields<typeof auth>()],
--- webapp/app/utils/env.ts
+++ webapp/app/utils/env.ts
@@ -1,6 +1,6 @@
-/**
- * Environment information
- */
+/// Environment information for the webapp to connect with the backend
+/// or anything else.
+///
 
 export function getBackendUrl() {
   return import.meta.env.VITE_BACKEND || "http://localhost:3000";
