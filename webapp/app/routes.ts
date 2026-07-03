cfc20847b9d3ebabec6beec0771976892e384f26
Rustum Zia <ziarustum@gmail.com>
2026-07-03 13:24:53 -0400

    Move layouts to separate directory

--- webapp/app/routes.ts
+++ webapp/app/routes.ts
@@ -8,7 +8,7 @@ import { redirect } from "react-router";
 
 export default [
   index("routes/home.tsx"),
-  route("admin", "routes/admin/layout.tsx", [
+  route("admin", "layouts/admin.tsx", [
     index("routes/admin/dashboard.tsx"),
     route("trainees", "routes/admin/trainees.tsx"),
     route("instructors", "routes/admin/instructors.tsx"),
@@ -16,15 +16,15 @@ export default [
     route("course-manager", "routes/admin/courseManager.tsx"),
     route("course-details/:courseId", "routes/admin/courseDetails.tsx"),
   ]),
-  route("trainee", "routes/trainee/layout.tsx", [
+  route("trainee", "layouts/trainee.tsx", [
     index("routes/trainee/home.tsx"),
     route("*", "routes/trainee/not-found.tsx"),
   ]),
-  route("instructor", "routes/instructor/layout.tsx", [
+  route("instructor", "layouts/instructor.tsx", [
     index("routes/instructor/home.tsx"),
     route("*", "routes/instructor/not-found.tsx"),
   ]),
-  layout("routes/auth/layout.tsx", [
+  layout("layouts/auth.tsx", [
     route("login", "routes/auth/login.tsx"),
     route("signup", "routes/auth/signup.tsx"),
     route("profile-select", "routes/auth/profile-select.tsx"),
