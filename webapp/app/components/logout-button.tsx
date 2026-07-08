acfb17ae9921a2fdd4f43392255e1f2abbf29063
Rustum Zia <ziarustum@gmail.com>
2026-07-08 14:45:27 -0400

    Invalidate queries after logging out

--- webapp/app/components/logout-button.tsx
+++ webapp/app/components/logout-button.tsx
@@ -1,25 +1,26 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 import { Button } from "~/components/ui/button";
 import { authClient } from "~/utils/auth";
 
 export function LogOutButton() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const logout = async function () {
     await authClient.signOut({
       fetchOptions: {
-        onSuccess: () => {
-          navigate("/login"); // redirect to login page
+        onSuccess: async () => {
+          await navigate("/login"); // redirect to login page
+          // If we don't invalidate all queries, it doesn't actually
+          // look like we've logged out
+          await queryClient.invalidateQueries();
         },
       },
     });
   };
 
   return (
-    <Button
-      className="!justify-start"
-      variant="link"
-      onClick={() => logout()}
-    >
+    <Button className="!justify-start" variant="link" onClick={() => logout()}>
       Log out
     </Button>
   );
