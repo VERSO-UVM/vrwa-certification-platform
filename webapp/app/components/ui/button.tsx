3673bcef5ba321fb5fa241dd0727448407867db4
Ellie <ejgookin@gmail.com>
2026-07-22 14:23:18 -0400

    Took out uncommented line

--- webapp/app/components/ui/button.tsx
+++ webapp/app/components/ui/button.tsx
@@ -12,7 +12,6 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "vrwa-light:bg-red-400/30 vrwa-light:hover:bg-red-400/50 vrwa-light:text-gray-600 hover:vrwa-light:text-gray-700 dark:bg-red-500/25 dark:hover:bg-red-500/40 dark:text-gray-300 dark:hover:text-gray-200",
-        // "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
