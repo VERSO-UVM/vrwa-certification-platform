1ef1909b9979b62e0f21c336de8ac667cb3d50cd
Rustum Zia <ziarustum@gmail.com>
2026-07-27 11:56:01 -0400

    Remove obsolete routes/ directory

--- webapp/app/routes.ts
+++ webapp/app/routes.ts
@@ -11,17 +11,17 @@ export default [
     index("pages/admin/dashboard.tsx"),
     route("trainees", "pages/admin/trainee-manager.tsx"),
     route("instructors", "pages/admin/user-manager.tsx"),
-    route("*", "routes/admin/not-found.tsx"),
+    route("*", "pages/admin/not-found.tsx"),
     route("course-manager", "pages/admin/course-manager.tsx"),
     route("course-details/:courseId", "pages/admin/course-details.tsx"),
   ]),
   route("trainee", "layouts/trainee.tsx", [
     index("pages/trainee/home.tsx"),
-    route("*", "routes/trainee/not-found.tsx"),
+    route("*", "pages/trainee/not-found.tsx"),
   ]),
   route("instructor", "layouts/instructor.tsx", [
     index("pages/instructor/home.tsx"),
-    route("*", "routes/instructor/not-found.tsx"),
+    route("*", "pages/instructor/not-found.tsx"),
   ]),
   layout("layouts/auth.tsx", [
     route("login", "pages/auth/login.tsx"),
--- webapp/app/routes/instructor/home.tsx
+++
@@ -1,6 +0,0 @@
-import { InstructorHome } from "~/instructor/home";
-import type { Route } from "../+types/home";
-
-export default function () {
-  return <InstructorHome />;
-}
