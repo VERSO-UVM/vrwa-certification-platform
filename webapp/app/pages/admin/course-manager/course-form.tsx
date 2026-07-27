efa809fe8df5ef51a724296e92ce142c693248df
Rustum Zia <ziarustum@gmail.com>
2026-07-27 13:27:49 -0400

    feelin good

--- webapp/app/pages/admin/course-manager/course-form.tsx
+++ webapp/app/pages/admin/course-manager/course-form.tsx
@@ -8,10 +8,10 @@ import {
   FieldLabel,
 } from "~/components/ui/field";
 import { Button } from "~/components/ui/button";
-import { Textarea } from "../../../components/ui/textarea";
 import type { Course } from "@backend/database/schema";
 import type { CourseUpdateInput } from "@backend/routers/course";
 import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
 
 //for input validation of tuition fee
 function textToDollars(userInput: string) {
