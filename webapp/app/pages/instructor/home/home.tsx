c6de59d113dddd433b766eaf4b2565cb1efe8a1d
Rustum Zia <ziarustum@gmail.com>
2026-08-06 13:18:24 -0400

    Improve instructor home page (#109)
    * instructor home: separate past and future classes
    
    * finish home page
    
    * fix: React Query serializing JavaScript types (#111)
    
    * Add SkeletonCard component
    
    * Instructor home page touch ups

--- webapp/app/pages/instructor/home/home.tsx
+++ webapp/app/pages/instructor/home/home.tsx
@@ -5,34 +5,134 @@ import { PageHeader } from "~/components/page-header";
 import { useTRPC } from "~/utils/trpc";
 import { useQuery } from "@tanstack/react-query";
 import { ClassInfoCard } from "./class-info-card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { DataTable } from "~/components/data-table";
+import {
+  courseEventDefs,
+  courseEventFieldHelper,
+} from "~/utils/field-defs/course-event";
+import { Link } from "react-router";
+import { useMemo } from "react";
+import { SkeletonCard } from "~/components/ui/skeleton";
 
 export function meta() {
   return [{ title: "Instructor Dashboard" }];
 }
+
+/**
+ * Use start of the day instead of the current time so that
+ * a class on the current day is still at the top of the page
+ * in Upcoming Classes.
+ */
+function startOfToday() {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  return today;
+}
+
+const columnDefs = [
+  courseEventFieldHelper.accessor("courseName", {
+    header: "Name",
+    cell: ({ row, getValue }) => (
+      <Link
+        to={{
+          pathname: `/instructor/attendance/${row.original.id}`,
+          search: `?view=edit`,
+        }}
+        className="font-medium hover:underline"
+      >
+        {getValue() as string}
+      </Link>
+    ),
+  }),
+  courseEventDefs.courseDate,
+  courseEventDefs.courseLocationType,
+  courseEventDefs.address,
+  courseEventDefs.seats,
+];
 
 export default function InstructorHome() {
   const trpc = useTRPC();
   const { data: courseEvents, isPending } = useQuery(
     trpc.courseEvents.instructor.listUpcoming.queryOptions(),
   );
+  const today = startOfToday();
+
+  const pastClasses = useMemo(
+    () =>
+      courseEvents?.filter(
+        ({ classStartDatetime }) =>
+          classStartDatetime != null && classStartDatetime < today,
+      ),
+    [courseEvents],
+  );
+  const upcomingClasses = useMemo(
+    () =>
+      (courseEvents ?? []).filter(({ classStartDatetime }) => {
+        // Consider a null classStartDatetime as a class which has not yet
+        // been scheduled. Thus it is a future class.
+        return classStartDatetime == null || classStartDatetime > today;
+      }),
+    [courseEvents],
+  );
 
   return (
-    <div className="space-y-6">
-      <PageHeader>Upcoming Classes</PageHeader>
+    <div>
+      <PageHeader>My Classes</PageHeader>
+
+      <h2 className="text-xl font-medium pb-5">Upcoming Classes</h2>
 
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {courseEvents?.map((session, i) => (
+        {upcomingClasses.map((session, i) => (
           <ClassInfoCard key={session.id} session={session} index={i} />
         ))}
-        {courseEvents?.length === 0 &&
+        {upcomingClasses.length === 0 &&
           (isPending ? (
-            <div className="p-10">Fetching upcoming classes...</div>
+            <>
+              <SkeletonCard variant="blue" />
+              <SkeletonCard variant="green" />
+              <SkeletonCard variant="yellow" />
+            </>
           ) : (
-            <div className="col-span-full py-20 text-center text-muted-foreground bg-muted/20 rounded-lg border-2 border-dashed">
+            <div className="col-span-full py-20 text-center text-muted-foreground bg-muted/20 rounded-lg">
               No upcoming classes right now!
             </div>
           ))}
       </div>
+
+      <h2 className="text-xl font-medium pt-8 pb-5">Past Classes</h2>
+
+      <Card className="@xl:col-span-5" variant="blue">
+        <CardHeader>
+          <CardTitle>Past Classes</CardTitle>
+          <CardDescription>
+            Click on a class name to manage attendance.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columnDefs}
+            data={pastClasses}
+            table={{
+              enableRowSelection: false,
+              initialState: {
+                sorting: [
+                  {
+                    id: "classStartDatetime",
+                    desc: true,
+                  },
+                ],
+              },
+            }}
+          />
+        </CardContent>
+      </Card>
     </div>
   );
 }
