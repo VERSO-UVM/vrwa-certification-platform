52c9273889ea2f663c3f516a010e14a18f6bfdcf
Ellie <ejgookin@gmail.com>
2026-08-07 14:04:24 -0400

    Added different logo option

--- webapp/app/components/sidebar-layout.tsx
+++ webapp/app/components/sidebar-layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import { SidebarProvider, SidebarTrigger, SidebarInset } from "./ui/sidebar";
 import { Separator } from "./ui/separator";
 import { Link } from "react-router";
+import { User } from "lucide-react";
 
 type SidebarLayoutProps = {
   sidebar: React.ComponentType;
@@ -14,13 +15,15 @@ export function SidebarLayout({ sidebar: Sidebar, homeUrl}: SidebarLayoutProps) 
       <SidebarProvider>
         <Sidebar />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <header className="flex w-full h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
+          <div className="flex w-60 h-16 items-center">
             <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            <Separator orientation="vertical" className="ml-1 mr-2 xl:ml-2 xl:mr-3 data-[orientation=vertical]:h-4" />
             <Link to={homeUrl}>
-              <img className="h-10 object-cover" src="../../app/logo.png" alt="VRWA Logo" />
+              <img className="h-10 object-cover" src="../../app/logo-dark.png" alt="VRWA Logo" />
             </Link>
-
+          </div>
+            <User className="size-7 bg-gray-400 rounded-full"/>
           </header>
           <div className="flex-1">
             {/* See https://tailwindcss.com/docs/responsive-design#container-queries
