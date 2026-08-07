db28271dec3d009c8fc05669634cc1ce28b20ebe
Rustum Zia <ziarustum@gmail.com>
2026-08-07 12:12:08 -0400

    feat(font): use Open Sans instead of Inter font family

--- webapp/app/root.tsx
+++ webapp/app/root.tsx
@@ -29,7 +29,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Quintessential&family=Vollkorn:ital,wght@0,400..900;1,400..900&display=swap",
   },
 ];
 
