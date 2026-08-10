9c6ce99c76e98bc65b7abc2b17f6dff4a05c9105
Rustum Zia <ziarustum@gmail.com>
2026-08-10 00:48:49 -0400

    Fix! tRPC batch requests being rejected by Fastify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,9 @@ import { appRouter } from "~/routers";
 import { TRUSTED_ORIGINS } from "./constants";
 const app = Fastify({
   logger: true,
+  // This needs to be high so tRPC batch requests aren't rejected
+  // See https://trpc.io/docs/server/adapters/fastify
+  maxParamLength: 5000,
 });
 
 app.register(cors, {
