37bcba9d66c6d3da2a158261a6968e615abc713a
Rustum Zia <ziarustum@gmail.com>
2026-08-12 15:32:05 -0400

    Update pa

--- webapp/app/components/payment-status-badge.tsx
+++ webapp/app/components/payment-status-badge.tsx
@@ -1,11 +1,19 @@
-import type { PaymentStatus } from "@backend/database/schema";
+import { PaymentStatus } from "@backend/database/schema";
 import { Badge } from "~/components/ui/badge";
 
 export function PaymentStatusBadge({ value }: { value: PaymentStatus }) {
   switch (value) {
-    case "paid":
+    case PaymentStatus.Paid:
       return <Badge variant="green">Paid</Badge>;
-    case "unpaid":
-      return <Badge variant="orange">Unpaid</Badge>;
+    case PaymentStatus.Draft:
+      return <Badge variant="blue">Draft</Badge>;
+    case PaymentStatus.Open:
+      return <Badge variant="blue">Open</Badge>;
+    case PaymentStatus.Refunded:
+      return <Badge variant="orange">Refunded</Badge>;
+    case PaymentStatus.Void:
+      return <Badge variant="indigo">Void</Badge>;
+    case PaymentStatus.Uncollectible:
+      return <Badge variant="orange">Uncollectible</Badge>;
   }
 }
